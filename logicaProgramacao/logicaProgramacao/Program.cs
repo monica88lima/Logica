46e2269aca00663c08289318a4caeecc96162e74
monica88lima <flima.monica@gmail.com>
2025-06-15 15:43:23 -0300

    20. Valid Parentheses - https://leetcode.com/problems/valid-parentheses/description/

---
+++ logicaProgramacao/logicaProgramacao/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using logicaProgramacao;
+
+Console.WriteLine("Hello, World!");
+bool Exercicio20 = ValidParentheses.IsValid("([)]");
+Console.WriteLine($"Exercicio 20: {Exercicio20}");
+Exercicio20 = ValidParentheses.IsValid("()[]{}");
+Console.WriteLine($"Exercicio 20: {Exercicio20}");
+Exercicio20 = ValidParentheses.IsValid("([])");
+Console.WriteLine($"Exercicio 20: {Exercicio20}");
---
+++ logicaProgramacao/logicaProgramacao/ValidParentheses.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicaProgramacao
+{
+    internal static class ValidParentheses
+    {
+        public static bool IsValid(string s)
+        { 
+            Stack<char> pilha= new Stack<char>();
+            if (string.IsNullOrEmpty(s) || s.Length % 2 != 0)
+            {
+                return false;
+            }
+
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    pilha.Push(c);
+                }
+                else
+                {
+                    if(pilha.Count > 0)
+                    {
+                        char ultimo = pilha.Pop();
+                        if ((ultimo == '(' && c != ')') ||
+                            (ultimo == '{' && c != '}') ||
+                            (ultimo == '[' && c != ']'))
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                   
+                }
+            }
+                      
+           
+
+            return true;
+        }
+    }
+}
